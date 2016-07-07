3b1f8dc4e35699eed0ca19d2367a9ce3a92864d1
Pekes317 <El_Dante565@yahoo.com>
2016-07-06 21:36:29 -0500

    Added Buttons Pic Form

--- app/pages/submit/forms/submit-pic.form.ts
+++ app/pages/submit/forms/submit-pic.form.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 
+import { PictureService } from '../../../services';
+import { MyLoader } from '../../shared/myloader';
+
 @Component({
 	selector: 'ebc-pic-form',
-	templateUrl : 'build/pages/submit/forms/submit-pic.form.html'
+	templateUrl: 'build/pages/submit/forms/submit-pic.form.html',
+	directives: [MyLoader],
+	providers: [PictureService]
 })
 export class PicForm {
-	art:string;
 
-	constructor() {
+	constructor(public pic: PictureService) {
 
 	}
 
