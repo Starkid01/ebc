c5d12341ed6eae26bb81e05742987552337ca657
Pekes317 <El_Dante565@yahoo.com>
2018-03-05 20:56:32 -0600

    fixed update

--- src/pages/profile/myperson/myperson.page.ts
+++ src/pages/profile/myperson/myperson.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { IonicPage } from 'ionic-angular';
+import { Events, IonicPage } from 'ionic-angular';
 
 import { BackandUser } from '../../../providers/backand';
 import { UserService } from '../../../providers/myservices';
@@ -17,10 +17,21 @@ export class PersonPage implements OnInit {
   editPage: string = 'edit';
   userData: BackandUser;
   
-  constructor(private user: UserService) { }
+  constructor(private events: Events, private user: UserService) { }
 
   ngOnInit() {
+    this.parseUser();
+    this.watchUpdate();
+  }
+
+  private parseUser() {
     this.user.getUser()
     .then(user => this.userData = user);
+  }
+
+  private watchUpdate() {
+    this.events.subscribe('myUser', () => {
+      this.parseUser();
+    })
   }
 }
