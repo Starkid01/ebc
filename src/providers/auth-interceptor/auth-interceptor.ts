87ad648431c19ae2dac17245e9629aec158e140b
Pekes317 <El_Dante565@yahoo.com>
2018-03-30 07:32:06 -0500

    2.3.3

--- src/providers/auth-interceptor/auth-interceptor.ts
+++ src/providers/auth-interceptor/auth-interceptor.ts
@@ -1,4 +1,4 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Observable } from 'rxjs/Observable';
@@ -9,14 +9,14 @@ export class AuthInterceptorProvider implements HttpInterceptor {
   constructor(public fireAuth: AngularFireAuth) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    let token = this.fireAuth.auth.currentUser ? this.fireAuth.auth.currentUser['qa'] : false;
+    let token = this.fireAuth.auth.currentUser ? this.fireAuth.auth.currentUser['qa'] : undefined;
+    let headers = new HttpHeaders();
     
+    headers.append('Content-Type', 'application/json; charset=utf-8');
+    headers.append('Accept', 'application/json');
+    if (token) headers.append('Authorization', `Bearer ${token}`);
     req = req.clone({
-      setHeaders: {
-        'Content-Type': 'application/json; charset=utf-8',
-        'Accept': 'application/json',
-        'Authorization': `Bearer ${token}`
-      },
+      headers: headers
     });
 
     return next.handle(req);
