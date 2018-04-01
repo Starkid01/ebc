a3ab594e1c18d6bef219d6f1670011840a87876a
Pekes317 <El_Dante565@yahoo.com>
2018-04-01 13:31:22 -0500

    Fixed Stupid Headers

--- src/providers/auth-interceptor/auth-interceptor.ts
+++ src/providers/auth-interceptor/auth-interceptor.ts
@@ -1,4 +1,4 @@
-import { HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Observable } from 'rxjs/Observable';
@@ -10,13 +10,14 @@ export class AuthInterceptorProvider implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let token = this.fireAuth.auth.currentUser ? this.fireAuth.auth.currentUser['qa'] : undefined;
-    let headers = new HttpHeaders();
+    let headers = {
+      'Content-Type': 'application/json; charset=utf-8',
+      'Accept': 'application/json'
+    }
     
-    headers.append('Content-Type', 'application/json; charset=utf-8');
-    headers.append('Accept', 'application/json');
-    if (token) headers.append('Authorization', `Bearer ${token}`);
+    if (token) headers['Authorization'] = `Bearer ${token}`;
     req = req.clone({
-      headers: headers
+      setHeaders: headers
     });
 
     return next.handle(req);
